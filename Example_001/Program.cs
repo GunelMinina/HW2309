4cece609e4c369ee5cec124ae2c28a81437efcdb
Gunel <gunelminina@gmail.com>
2022-10-01 00:10:01 +0300

    Zadacha 19

---
+++ Example_001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+Console.WriteLine("введите пятизначное число");
+string numbr = Console.ReadLine();
+
+if (numbr[0] == numbr[4] & numbr[1] == numbr[3]) 
+    {   
+    Console.WriteLine("Palindrom");
+    }
+else
+    {
+    Console.WriteLine("NE Palindrom");
+    }
